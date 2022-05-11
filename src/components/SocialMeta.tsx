65278d88e9f57ce32688e0933ae5b82f8f123a92
Sam King <mail@samking.co>
2022-05-11 16:27:24 +0100

    Change to large image card in the social metadata

--- src/components/SocialMeta.tsx
+++ src/components/SocialMeta.tsx
@@ -34,7 +34,7 @@ export function SocialMeta({
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={ogImage} />
-      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@samkingco" />
     </Head>
   );
