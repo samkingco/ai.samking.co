f482004607398841928b1f378cc4906976a0fbf5
Sam King <mail@samking.co>
2022-05-11 15:50:34 +0100

    Clean up graphql client stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,11 @@ import "@reach/dialog/styles.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import React from "react";
-import { Provider as GraphqlProvider } from "urql";
-import { client } from "../graphql";
 import "../styles/globals.css";
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <GraphqlProvider value={client}>
+    <>
       <Head>
         <script
           defer
@@ -17,6 +15,6 @@ export default function CustomApp({ Component, pageProps }: AppProps) {
         ></script>
       </Head>
       <Component {...pageProps} />
-    </GraphqlProvider>
+    </>
   );
 }
